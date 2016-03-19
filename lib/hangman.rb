9e55bcf10922ce575bbc60253239d1438116a343
Daniel Magro <dmagro04@gmail.com>
2016-03-19 16:48:11 +0000

    Choosing secret word method implemented and some method definition

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -5,6 +5,7 @@ class Hangman
   OUTPUT_MSGS = { 
     ask_for_file: "There is no such file. Do you know where is the dictionary?",
     file_not_found: "Nope, file also not found. Exiting...",
+    no_dict: "No dictionary available. Exiting...",
     lose_msg: ", sorry mate..."
   }
 
@@ -13,22 +14,37 @@ class Hangman
 
   def initialize(dictionary_path)
     @dictionary_path = dictionary_path
-    @dictionary = load_dictionary()
-    @secret_word = choose_word()
+    load_dictionary()
+    choose_word()
+    @guess_fails = 0
   end
   
   public
 
+  def play_game()
+    #GAME INTERPRETER
+  end
+
   private
+  def game_reset()
+    choose_word
+    @guess_fails = 0
+  end
+
+  def valid_word?(word)
+    word.length.between?(MIN_LEN, MAX_LEN)
+  end
+
   def ask_for_file()
     puts OUTPUT_MSGS[:ask_for_file]
     file_path = gets.chomp.tr(" \t\n", '')
   end
 
   def load_dictionary()
+    @dictionary = {}
     begin
       filename = File.open(@dictionary_path, "r")
-      filename.readlines.inject({}) {|hash,l| hash[l.chomp.to_sym]=l.chomp if l.chomp.length.between?(MIN_LEN, MAX_LEN); hash}
+      filename.readlines.inject(@dictionary){ |hash,line| word = line.chomp.downcase; hash[word.to_sym] = word if valid_word?(word); hash}
       filename.close
     rescue Errno::ENOENT
       file_path = ask_for_file()
@@ -43,11 +59,25 @@ class Hangman
   end
 
   def choose_word()
-    
+    if @dictionary.empty?
+      puts OUTPUT_MSGS[no_dict]
+      Process.exit
+    end
+    words = @dictionary.values
+    @secret_word = words[rand(words.size)]
+  end
+
+  def save_game(filename)
+    #SAVES THE CURRENT GAME STATE
+  end
+
+  def load_game(filename)
+    #LOADS THE SAVED GAME STATE
   end
 end
 
 #MAIN
 if __FILE__ == $0
   game = Hangman.new("5desk.txt")
+
 end
