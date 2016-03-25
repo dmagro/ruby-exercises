0cbeb0499701675deaefd974ca902e11b530279d
Daniel Magro <dmagro04@gmail.com>
2016-03-25 17:50:42 +0000

    Iteration 3 completed

--- lib/micro_blogger.rb
+++ lib/micro_blogger.rb
@@ -17,6 +17,31 @@ class MicroBlogger
     end
   end
 
+  def followers_list
+    @client.followers.collect { |follower| @client.user(follower).screen_name }
+  end
+
+  def is_follower?(target)
+    screen_names = followers_list
+    screen_names.include? target
+  end
+
+  def dm(target, message)
+    if is_follower? target
+      puts "Trying to send #{target} this direct message:"
+      puts message
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts "Sorry this person doesn't follow you..."
+    end 
+  end
+
+  def spam_my_followers(message)
+    followers = followers_list
+    followers.each { |follower| dm(follower, message) }
+  end
+
   def run
     puts "Welcome to the JSL Twitter Client!"
     command = ""
@@ -29,6 +54,8 @@ class MicroBlogger
       case command
       when 'q' then puts "Goodbye!"
       when 't' then tweet(parts[1..-1].join(" "))
+      when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+      when 'spam' then spam_my_followers(parts[1..-1].join(" "))
       else
         puts "Sorry, I don't know how to #{command}"
       end
